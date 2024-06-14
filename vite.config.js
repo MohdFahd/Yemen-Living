5cd8ee94ef2d10af284d182e6addfc0e7081962b
mohdfahd <mohammed.f.alsulami@gmail.com>
2024-06-14 06:45:51 -0600

    refactor: Update base URL in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/YemenLiving/", // Set your base URL here
+  base: "./", // Set your base URL here
   plugins: [vue()],
 });
