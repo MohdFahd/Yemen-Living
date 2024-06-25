007b15aa21d82caac448f7b0ac4bfd828da28c49
mohdfahd <mohammed.f.alsulami@gmail.com>
2024-06-25 01:29:43 -0600

    refactor: Update vector in hero component and optimize page load performance with lazy loading for images

--- src/components/hero.vue
+++ src/components/hero.vue
@@ -33,7 +33,7 @@
     class="flex justify-center max-[1023px]:justify-center items-start relative overflow-hidden max-md:mt-16"
     ref="image"
   >
-    <vector class="absolute top-20" />
+    <vector class="absolute top-20 w-[80%] max-md:w-[70%] animate-spin-slow" />
     <img src="../assets/imgs/hero.webp" class="z-10" width="60%" alt="" />
   </div>
 </template>
