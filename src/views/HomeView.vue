417807a593ce9df42722431c3eaac1253b21bf50
mohdfahd <mohammed.f.alsulami@gmail.com>
2024-06-27 11:08:29 -0600

    change deley

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,21 +12,19 @@ const runAnimation = async () => {
   const curve = "M0 502S175 272 500 272s500 230 500 230V0H0Z";
   const flat = "M0 2S175 1 500 1s500 1 500 1V0H0Z";
   tl.from(".loader-wrap-heading h1", {
-    delay: 0.5,
     y: 200,
     skewY: 10,
   })
     .to(".loader-wrap-heading h1", {
-      delay: 1.5,
       y: -200,
     })
     .to(".svg path", {
-      duration: 0.8,
+      duration: 0.5,
       attr: { d: curve },
       ease: "power2.easeIn",
     })
     .to(".svg path", {
-      duration: 0.8,
+      duration: 0.5,
       attr: { d: flat },
       ease: "power2.easeOut",
       onComplete: () => {
