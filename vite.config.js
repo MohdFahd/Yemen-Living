f7bccf8153332855d378abe4098d216e1e689c41
mohdfahd <mohammed.f.alsulami@gmail.com>
2024-06-27 07:30:13 -0600

    dep

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Yemen-Living/", // Set your base URL here
+  base: "./", // Set your base URL here
   plugins: [vue()],
 });
