f8f3f6e95b003691d6b785b2f202de5b4d92a70c
mohdfahd <mohammed.f.alsulami@gmail.com>
2024-06-27 07:57:23 -0600

    deploy

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "./", // Set your base URL here
-  plugins: [vue()],
-});
